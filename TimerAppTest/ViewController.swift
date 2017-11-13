93a5b40eca4b3ffd24f56c8208eeb9263ef970fa
Daniel Suskevic <dsuskevic@klt-applemacmookpro1.local>
2017-11-13 21:59:42 +0200

    Timer with text field (seconds)

--- TimerAppTest/ViewController.swift
+++ TimerAppTest/ViewController.swift
@@ -8,10 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    var timer = Timer()
+    var number : Int = 0
+    var state : Bool = true
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var inputTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        inputTextField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +27,56 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func pause() {
+        timer.invalidate()
+    }
+    
+    @IBAction func tap(_ sender: UITapGestureRecognizer) {
+        if state == false {
+            state = true
+            sTimer()
+        } else if state == true {
+            state = false
+            pause()
+        }
+    }
+    
+    // Mark: Timers
+    
+    func sTimer() {
+    timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        check()
+        if state == true {
+        number -= 1
+        label.text = String(number)
+        }
+    }
+    
+    func check() {
+        if number < 1 {
+            timer.invalidate()
+            state = false
+        }
+    }
+    
+    //MARK: Text Field Delegate Methods
 
-
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        label.text = inputTextField.text
+        if let a = Int(inputTextField.text!) {
+            number = a
+        }
+        print("hoooli", textField)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
 }
 
